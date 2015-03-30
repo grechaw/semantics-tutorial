694a043da8d1f0a2f32a7c877b7c5daf4de318a9
Charles Greer <cgreer@marklogic.com>
2015-03-30 15:10:27 -0700

    semantics app, two controllers

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 MLCP="./mlcp-Hadoop2-1.3-1/bin/mlcp.sh"
 USERNAME=admin
 PASSWORD=admin
-CURL="curl -i -X PUT --digest --user $USERNAME:$PASSWORD"
+CURL="curl -X PUT --digest --user $USERNAME:$PASSWORD"
 PWD=`pwd`
 
 # setup the database and apperserver.
@@ -16,14 +16,15 @@ $CURL -Hcontent-type:application/json -d@database-properties.json "http://localh
 $CURL -Hcontent-type:text/html -d@tutorial.html http://localhost:8000/v1/documents?uri=tutorial.html 
 
 # intall an extension that can be the web server for this application
+# this is the "middle tier" that serves the HTML pages for the application.
 $CURL -Hcontent-type:application/javascript -d@htmlServer.sjs http://localhost:8000/v1/config/resources/htmlServer ; 
 
 # install the extension to be called by angular
 $CURL -Hcontent-type:application/javascript -d@semanticExtension.sjs http://localhost:8000/v1/config/resources/semanticExtension ; 
 
 # import RDF data
-$MLCP IMPORT -input_file_path data/rdf -input_file_type rdf -username $USERNAME -password $PASSWORD -host localhost -port 8000
-# import JSON data
-$MLCP IMPORT -input_file_path data/json -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/json,''"
-$MLCP IMPORT -input_file_path data/xml -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml,''"
-
+#$MLCP IMPORT -input_file_path data/rdf -input_file_type rdf -username $USERNAME -password $PASSWORD -host localhost -port 8000
+## import JSON data
+#$MLCP IMPORT -input_file_path data/json -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/json,''"
+#$MLCP IMPORT -input_file_path data/xml -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml,''"
+#
