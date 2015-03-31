a8a610e36dbc13c94be95a8c7934812b8963dee9
Charles Greer <cgreer@marklogic.com>
2015-03-31 16:37:18 -0700

    Angular changes to render results

--- setup.sh
+++ setup.sh
@@ -10,21 +10,28 @@ PWD=`pwd`
 # these two steps enable the triples index and can set the inference
 # size.   See config/appserver-properties.json and config/database-properties.json
 # These really only need to be done once
+echo "Configuring database"
 $CURL -Hcontent-type:application/json -d@database-properties.json "http://localhost:8002/manage/v2/databases/Documents/properties?group-id=Default"
 
 # installs the tutorial html page on MarkLogic REST server as a document.
+echo "Installing HTML page"
 $CURL -Hcontent-type:text/html -d@tutorial.html http://localhost:8000/v1/documents?uri=tutorial.html 
 
 # intall an extension that can be the web server for this application
 # this is the "middle tier" that serves the HTML pages for the application.
+echo "Installing HTML page server extension"
 $CURL -Hcontent-type:application/javascript -d@htmlServer.sjs http://localhost:8000/v1/config/resources/htmlServer ; 
 
 # install the extension to be called by angular
+echo "Installing the semantic extension"
 $CURL -Hcontent-type:application/javascript -d@semanticExtension.sjs http://localhost:8000/v1/config/resources/semanticExtension ; 
 
 # import RDF data
+echo "Loading RDF Data..."
 $MLCP IMPORT -input_file_path data/rdf -input_file_type rdf -username $USERNAME -password $PASSWORD -host localhost -port 8000
 ## import JSON data
+echo "Loading JSON..."
 $MLCP IMPORT -input_file_path data/json -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/json,''"
+echo "Loading XML..."
 $MLCP IMPORT -input_file_path data/xml -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml,''"
 #
