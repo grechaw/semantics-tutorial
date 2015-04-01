4db1c4d6b3a635b0ecdffdb1ca936ec8b7e889bc
Charles Greer <cgreer@marklogic.com>
2015-04-01 11:48:15 -0700

    angular accordion style

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ PASSWORD=admin
 CURL="curl -X PUT --digest --user $USERNAME:$PASSWORD"
 PWD=`pwd`
 
-# setup the database and apperserver.
+## setup the database and apperserver.
+
 # these two steps enable the triples index and can set the inference
-# size.   See config/appserver-properties.json and config/database-properties.json
-# These really only need to be done once
+# Really only needs to be done once
 echo "Configuring database"
 $CURL -Hcontent-type:application/json -d@database-properties.json "http://localhost:8002/manage/v2/databases/Documents/properties?group-id=Default"
 
@@ -22,16 +22,19 @@ $CURL -Hcontent-type:text/html -d@tutorial.html http://localhost:8000/v1/documen
 echo "Installing HTML page server extension"
 $CURL -Hcontent-type:application/javascript -d@htmlServer.sjs http://localhost:8000/v1/config/resources/htmlServer ; 
 
-# install the extension to be called by angular
 echo "Installing the semantic extension"
 $CURL -Hcontent-type:application/javascript -d@semanticExtension.sjs http://localhost:8000/v1/config/resources/semanticExtension ; 
 
-# import RDF data
-echo "Loading RDF Data..."
-$MLCP IMPORT -input_file_path data/rdf -input_file_type rdf -username $USERNAME -password $PASSWORD -host localhost -port 8000
-## import JSON data
+##
+## data imports
+##
+
+#echo "Loading RDF Data..."
+#$MLCP IMPORT -input_file_path data/rdf -input_file_type rdf -username $USERNAME -password $PASSWORD -host localhost -port 8000
 echo "Loading JSON..."
 $MLCP IMPORT -input_file_path data/json -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/json,''"
 echo "Loading XML..."
-$MLCP IMPORT -input_file_path data/xml -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml,''"
+$MLCP IMPORT -input_file_path data/xml/matches -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml,''"
+echo "Loading XML Triples Docs..."
+$MLCP IMPORT -input_file_path data/xml/triples -username $USERNAME -password $PASSWORD -host localhost -port 8000 -output_uri_replace "$PWD/data/xml/triples,''" -output_collections "http://marklogic.com/semantics#default-graph" 
 #
