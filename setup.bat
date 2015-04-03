8dbe88f901f3bc2724f4bc4ba2bc2445466e0c0f
Charles Greer <cgreer@marklogic.com>
2015-04-03 16:38:53 -0700

    extensions, final data load

--- setup.bat
+++ setup.bat
@@ -14,20 +14,15 @@ echo "Installing HTML page server extension"
 %CURL% -Hcontent-type:application/javascript -d@htmlServer.sjs http://localhost:8000/v1/config/resources/htmlServer ; 
 
 
-echo "Loading RDF Data..."
-%MLCP% IMPORT -input_file_path data/rdf -input_file_type rdf -username %USERNAME% -password %PASSWORD% -host localhost -port 8000
-echo "Loading JSON..."
-%MLCP% IMPORT -input_file_path data/json -username %USERNAME% -password %PASSWORD% -host localhost -port 8000 -output_uri_replace "%PWD%/data/json,''"
-echo "Loading XML..."
-%MLCP% IMPORT -input_file_path data/xml/matches -username %USERNAME% -password %PASSWORD% -host localhost -port 8000 -output_uri_replace "%PWD%/data/xml,''"
-echo "Loading XML Triples Docs..."
-%MLCP% IMPORT -input_file_path data/xml/triples -username %USERNAME% -password %PASSWORD% -host localhost -port 8000 -output_uri_replace "%PWD%/data/xml/triples,''" -output_collections "http://marklogic.com/semantics#default-graph" 
+echo "loading data..."
+%MLCP% IMPORT -input_file_path data -username %USERNAME% -password %PASSWORD% -host localhost -port 8000 -output_uri_replace "/%PWD%data/json,'',/%PWD%data/xml,'',/%PWD%data/xml/triples,''" -output_collections "http://marklogic.com/semantics#default-graph"
 
 
 rem installs the tutorial html page on MarkLogic REST server as a document.
 echo "Installing HTML page"
 %CURL% -Hcontent-type:text/html -d@tutorial.html http://localhost:8000/v1/documents?uri=tutorial.html 
 
-echo "Installing the semantic extension"
+echo "Installing the semantic extensions"
+%CURL% -Hcontent-type:application/javascript -d@inferringQuery.sjs http://localhost:8000/v1/config/resources/inferringQuery ; 
 %CURL% -Hcontent-type:application/javascript -d@semanticExtension.sjs http://localhost:8000/v1/config/resources/semanticExtension ; 
 
